98a1fa992120e1d744d1ca335ecad4033eaaed8f
Baktybekov <baktybekov2604@icloud.com>
2026-05-20 14:35:19 +0600

    reset1

--- delivery_store/settings.py
+++ delivery_store/settings.py
@@ -102,11 +102,11 @@ WSGI_APPLICATION = 'delivery_store.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default=os.environ.get('DATABASE_URL')
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
